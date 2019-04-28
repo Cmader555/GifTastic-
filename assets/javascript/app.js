9094ebb3bf396d4639925c53358cd65f32591373
Christian Mader <Cmader555@users.noreply.github.com>
2019-04-27 23:04:11 -0700

    App working properly, needs stling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 
 //Team array
 
-var teams = ["Arizona Cardinals", "Arizona Diamonbacks", "Arizona State Football", "Arizona State Basketball", "Pheonix Suns", "Arizona Coyotes"];
+var teams = ["Arizona Cardinals", "Arizona Diamonbacks", "Arizona State Football", "Arizona State Basketball", "Phoenix Suns", "Arizona Coyotes"];
 
 
 
@@ -13,7 +13,13 @@ function generateButton() {
     //loop to go through the array
     for (i = 0; i < teams.length; i++) {
         //creates button and adds value of team array value
-        $("#teamButtons").append(`<button type="button" class="btn btn-danger teamButton" value="${teams[i]}"> ${teams[i]} </button>`);
+        console.log(teams[i]);
+        //$("#teamButtons").append(`<button type="button" class="btn btn-danger teamButton"> ${teams[i]} </button>`);
+        var button = $("<button>");
+        button.attr("class", "btn btn-outline-danger teamButton btn-space");
+        button.attr("data-name", teams[i]);
+        button.html(teams[i]);
+        $("#teamButtons").append(button);
     };
 };
 
@@ -34,23 +40,50 @@ $("#add-team").on("click", function (event) {
 function gifGenerator() {
 
 
-    var nTeam = $(this).attr("value");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=HMiONM4iBo0PrAJ5528VESMF7ZWMRUdX&q=" + nTeam + "&limit=10&offset=0&rating=Y&lang=en";
-    console.log(nTeam); 
-    console.log(queryURL); 
+    var nTeam = $(this).attr("data-name");
+    console.log(nTeam);
+    //nTeam = nTeam.trim().replace(/ /g, "+")
+    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=HMiONM4iBo0PrAJ5528VESMF7ZWMRUdX&q=${nTeam}&limit=10&offset=0&rating=G&lang=en`; 
+
+    console.log(queryURL);
     // Creates AJAX call for the specific movie button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        
+        console.log(response.data)
+        $("#gifs").empty(); 
+        for (i = 0; i < response.data.length; i++) {
 
-        $("#gifs").append(response.url); 
+            var gif = $("<img>").attr("src", response.data[i].images.original_still.url);
+            gif.attr("class", "gif"); 
+            gif.attr("other", response.data[i].images.original.url); 
+            
+            $("#gifs").append(gif);
+            var rating = $(`<p> Rating: ${response.data[i].rating} </p>`);
+            $("#gifs").append(rating);
+
+        };
 
     });
 
 
 
 };
+
+
+$(document).on("click", ".gif",function () {
+
+    var source = $(this).attr("src"); 
+    var other = $(this).attr("other"); 
+    $(this).attr("src", other); 
+    $(this).attr("other", source); 
+
+
+
+}); 
+
 
 $(document).on("click", ".teamButton", gifGenerator);
 
